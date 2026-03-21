2001c0a11e8dc136506dbd0d3edbb9c79d6b3867
Karl Born <karlborn@bornadvisory.com>
2026-03-21 12:47:43 -0700

    Add initial Rust hello-world component

---
+++ rust_core/target/debug/deps/rust_core-f69af5acfde3872c.d
@@ -0,0 +1,5 @@
+/Users/karlborn/Development/projects/filesteward_hello/rust_core/target/debug/deps/rust_core-f69af5acfde3872c.d: src/main.rs
+
+/Users/karlborn/Development/projects/filesteward_hello/rust_core/target/debug/deps/rust_core-f69af5acfde3872c: src/main.rs
+
+src/main.rs:
---
+++ rust_core/target/debug/rust_core.d
@@ -0,0 +1,1 @@
+/Users/karlborn/Development/projects/filesteward_hello/rust_core/target/debug/rust_core: /Users/karlborn/Development/projects/filesteward_hello/rust_core/src/main.rs
